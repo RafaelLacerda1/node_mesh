5c9fd6b3cbb748106e50695df6ddc8ddb7db1647
Rafael Lacerda <rafaellacerda.ambiental@gmail.com>
2026-06-10 11:58:33 +0000

    chore: desabilita debug do flask

--- run.py
+++ run.py
@@ -4,9 +4,13 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # Garante que a pasta de dados existe
-    if not os.path.exists('data'):
-        os.makedirs('data')
-        
+    if not os.path.exists("data"):
+        os.makedirs("data")
+
     print("--> Iniciando Monitor de Nós Modular...")
-    app.run(host="0.0.0.0", port=8080, debug=True)
+
+    app.run(
+        host="0.0.0.0",
+        port=8080,
+        debug=False
+    )
